be390b3f3d80cb76ef9942bc6f48856f656ee667
nsahinbay <13198998+nsahinbay@users.noreply.github.com>
2025-03-17 16:36:20 +0300

    adding js bridge

--- src/android/su/scat/calltaxi/NotificationManagerPlugin.java
+++ src/android/su/scat/calltaxi/NotificationManagerPlugin.java
@@ -1,131 +1,123 @@
-package su.scat.calltaxi;
-
-import android.annotation.TargetApi;
-import android.app.Activity;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.content.Context;
-import android.content.Intent;
-import android.os.Build;
-import android.provider.Settings;
-
-import org.apache.cordova.CallbackContext;
-import org.apache.cordova.CordovaPlugin;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-public class NotificationManagerPlugin extends CordovaPlugin {
-
-    @TargetApi(26)
-    private void openAppNotificationSettings() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            final Activity activity = this.cordova.getActivity();
-            Intent intent = new Intent(Settings.ACTION_APP_NOTIFICATION_SETTINGS);
-            intent.putExtra(Settings.EXTRA_APP_PACKAGE, activity.getPackageName());
-            activity.startActivity(intent);
+var NotificationChannel = function(channelJSON) {
+    for (var property in channelJSON) {
+        if (channelJSON.hasOwnProperty(property)) {
+            this[property] = channelJSON[property];
         }
     }
 
-    @TargetApi(24)
-    private boolean areNotificationsEnabled() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            final Activity activity = this.cordova.getActivity();
-            final NotificationManager notificationManager = (NotificationManager) activity
-                    .getSystemService(Context.NOTIFICATION_SERVICE);
-            return notificationManager != null && notificationManager.areNotificationsEnabled();
-        }
-        return true;
-    }
+    this.openSettings = function() {
+        return NotificationManager.openNotificationChannelSettings(this.id);
+    };
 
-    @TargetApi(26)
-    private void openNotificationChannelSettings(String channelId) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            final Activity activity = this.cordova.getActivity();
-            Intent intent = new Intent(Settings.ACTION_CHANNEL_NOTIFICATION_SETTINGS);
-            intent.putExtra(Settings.EXTRA_APP_PACKAGE, activity.getPackageName());
-            intent.putExtra(Settings.EXTRA_CHANNEL_ID, channelId);
-            activity.startActivity(intent);
-        }
-    }
+    this._openSettings = function(onSuccess, onFail) {
+        NotificationManager._openNotificationChannelSettings(this.id, onSuccess, onFail);
+    };
+};
 
-    @TargetApi(26)
-    private JSONObject getNotificationChannel(String channelId) throws JSONException {
-        JSONObject channelJSON = new JSONObject();
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            final Activity activity = this.cordova.getActivity();
-            final NotificationManager manager = (NotificationManager) activity
-                    .getSystemService(Context.NOTIFICATION_SERVICE);
-            final NotificationChannel channel = manager.getNotificationChannel(channelId);
+var NotificationManager = function() { };
 
-            if (channel != null) {
-                channelJSON.put("id", channel.getId());
-                channelJSON.put("group", channel.getGroup());
-                channelJSON.put("description", channel.getDescription());
-                channelJSON.put("importance", channel.getImportance());
-                channelJSON.put("lightColor", channel.getLightColor());
-                channelJSON.put("lockscreenVisibility", channel.getLockscreenVisibility());
-            }
-        }
-        return channelJSON;
-    }
+function statusSuccess(status) {
+    console.log(status);
+};
 
-    @TargetApi(26)
-    private void createNotificationChannel(String channelId, String name, String description, int importance) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            final Activity activity = this.cordova.getActivity();
-            final NotificationManager notificationManager = (NotificationManager) activity
-                    .getSystemService(Context.NOTIFICATION_SERVICE);
+function statusError() {
+    console.log("Something went wrong");
+};
 
-            if (notificationManager != null) {
-                NotificationChannel channel = new NotificationChannel(channelId, name, importance);
-                channel.setDescription(description);
-                notificationManager.createNotificationChannel(channel);
-            }
-        }
-    }
+NotificationManager.SERVICE_NAME = 'NotificationManagerPlugin';
 
-    @Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        try {
-            if ("getNotificationChannel".equals(action)) {
-                callbackContext.success(getNotificationChannel(args.getString(0)));
-                return true;
-            }
+/**
+ * Notification Importance Levels (Android NotificationManager)
+ */
+NotificationManager.IMPORTANCE_DEFAULT = 3;
+NotificationManager.IMPORTANCE_HIGH = 4;
+NotificationManager.IMPORTANCE_LOW = 2;
+NotificationManager.IMPORTANCE_MAX = 5;
+NotificationManager.IMPORTANCE_MIN = 1;
+NotificationManager.IMPORTANCE_NONE = 0;
+NotificationManager.IMPORTANCE_UNSPECIFIED = -1000;
 
-            if ("openNotificationChannelSettings".equals(action)) {
-                openNotificationChannelSettings(args.getString(0));
-                callbackContext.success();
-                return true;
-            }
+/**
+ * Get notification channel details
+ * @param {string} channelId
+ * @returns {Promise<NotificationChannel>}
+ */
+NotificationManager.getNotificationChannel = function(channelId) {
+    return new Promise(function(onSuccess, onFail) {
+        NotificationManager._getNotificationChannel(channelId, onSuccess, onFail);
+    });
+};
 
-            if ("openAppNotificationSettings".equals(action)) {
-                openAppNotificationSettings();
-                callbackContext.success();
-                return true;
-            }
+/**
+ * Open App Notification Settings
+ * @returns {Promise}
+ */
+NotificationManager.openAppNotificationSettings = function() {
+    return new Promise(function(onSuccess, onFail) {
+        NotificationManager._openAppNotificationSettings(onSuccess, onFail);
+    });
+};
 
-            if ("areNotificationsEnabled".equals(action)) {
-                JSONObject s = new JSONObject();
-                s.put("status", areNotificationsEnabled());
-                callbackContext.success(s);
-                return true;
-            }
+/**
+ * Open Notification Channel Settings
+ * @param {string} channelId
+ * @returns {Promise}
+ */
+NotificationManager.openNotificationChannelSettings = function(channelId) {
+    return new Promise(function(onSuccess, onFail) {
+        NotificationManager._openNotificationChannelSettings(channelId, onSuccess, onFail);
+    });
+};
 
-            if ("createNotificationChannel".equals(action)) {
-                String channelId = args.getString(0);
-                String name = args.getString(1);
-                String description = args.getString(2);
-                int importance = args.getInt(3);
-                createNotificationChannel(channelId, name, description, importance);
-                callbackContext.success();
-                return true;
-            }
+/**
+ * Create a new Notification Channel
+ * @param {string} channelId - Unique ID for the channel
+ * @param {string} name - Name of the channel
+ * @param {string} description - Description of the channel
+ * @param {number} importance - Importance level of the channel (see NotificationManager.IMPORTANCE_* constants)
+ * @returns {Promise}
+ */
+NotificationManager.createNotificationChannel = function(channelId, name, description, importance) {
+    return new Promise(function(onSuccess, onFail) {
+        NotificationManager._createNotificationChannel(channelId, name, description, importance, onSuccess, onFail);
+    });
+};
 
-        } catch (JSONException e) {
-            callbackContext.error(e.getMessage());
-        }
+/**
+ * Check if notifications are enabled
+ * @returns {Promise}
+ */
+NotificationManager.areNotificationsEnabled = function() {
+    return new Promise(function(onSuccess, onFail) {
+        NotificationManager._areNotificationsEnabled(onSuccess, onFail);
+    });
+};
 
-        return false;
-    }
-}
+/**
+ * Private methods for Cordova exec calls
+ */
+NotificationManager._getNotificationChannel = function(channelId, onSuccess, onFail) {
+    cordova.exec(function(channelJSON) {
+        onSuccess(new NotificationChannel(channelJSON));
+    }, onFail, NotificationManager.SERVICE_NAME, 'getNotificationChannel', [channelId]);
+};
+
+NotificationManager._openAppNotificationSettings = function(onSuccess, onFail) {
+    cordova.exec(onSuccess, onFail, NotificationManager.SERVICE_NAME, 'openAppNotificationSettings');
+};
+
+NotificationManager._openNotificationChannelSettings = function(channelId, onSuccess, onFail) {
+    cordova.exec(function(channelJSON) {
+        onSuccess(new NotificationChannel(channelJSON));
+    }, onFail, NotificationManager.SERVICE_NAME, 'openNotificationChannelSettings', [channelId]);
+};
+
+NotificationManager._createNotificationChannel = function(channelId, name, description, importance, onSuccess, onFail) {
+    cordova.exec(onSuccess, onFail, NotificationManager.SERVICE_NAME, 'createNotificationChannel', [channelId, name, description, importance]);
+};
+
+NotificationManager._areNotificationsEnabled = function(onSuccess, onFail) {
+    cordova.exec(onSuccess, onFail, NotificationManager.SERVICE_NAME, 'areNotificationsEnabled');
+};
+
+module.exports = NotificationManager;
--- www/js/NotificationManager.js
+++ www/js/NotificationManager.js
@@ -1,91 +1,101 @@
 var NotificationChannel = function(channelJSON) {
-
-        for (var property in channelJSON)
-        {
-            if (channelJSON.hasOwnProperty(property))
-            {
-                this[property] = channelJSON[property];
-            }
+    for (var property in channelJSON) {
+        if (channelJSON.hasOwnProperty(property)) {
+            this[property] = channelJSON[property];
         }
+    }
 
-        this.openSettings = function()
-        {
-            return NotificationManager.openNotificationChannelSettings(this.id);
-        };
-
-        this._openSettings = function(onSuccess, onFail) {
-            NotificationManager._openNotificationChannelSettings(this.id, onSuccess, onFail);
-        };
-    },
-    NotificationManager = function() {
+    this.openSettings = function() {
+        return NotificationManager.openNotificationChannelSettings(this.id);
     };
+
+    this._openSettings = function(onSuccess, onFail) {
+        NotificationManager._openNotificationChannelSettings(this.id, onSuccess, onFail);
+    };
+};
+
+var NotificationManager = function() { };
 
 function statusSuccess(status) {
     console.log(status);
 };
 
 function statusError() {
-    console.log("something went wrong");
+    console.log("Something went wrong");
 };
 
 NotificationManager.SERVICE_NAME = 'NotificationManagerPlugin';
+
 /**
- * @see https://developer.android.com/reference/android/app/NotificationManager.html#IMPORTANCE_DEFAULT
- * @type {number}
+ * Notification Importance Levels (Android NotificationManager)
  */
 NotificationManager.IMPORTANCE_DEFAULT = 3;
-/**
- * @see https://developer.android.com/reference/android/app/NotificationManager.html#IMPORTANCE_HIGH
- * @type {number}
- */
 NotificationManager.IMPORTANCE_HIGH = 4;
-/**
- * @see https://developer.android.com/reference/android/app/NotificationManager.html#IMPORTANCE_LOW
- * @type {number}
- */
 NotificationManager.IMPORTANCE_LOW = 2;
-/**
- * @see https://developer.android.com/reference/android/app/NotificationManager.html#IMPORTANCE_MAX
- * @type {number}
- */
 NotificationManager.IMPORTANCE_MAX = 5;
-/**
- * @see https://developer.android.com/reference/android/app/NotificationManager.html#IMPORTANCE_MIN
- * @type {number}
- */
 NotificationManager.IMPORTANCE_MIN = 1;
-/**
- * @see https://developer.android.com/reference/android/app/NotificationManager.html#IMPORTANCE_NONE
- * @type {number}
- */
 NotificationManager.IMPORTANCE_NONE = 0;
-/**
- * @see https://developer.android.com/reference/android/app/NotificationManager.html#IMPORTANCE_UNSPECIFIED
- * @type {number}
- */
 NotificationManager.IMPORTANCE_UNSPECIFIED = -1000;
 
-
+/**
+ * Get notification channel details
+ * @param {string} channelId
+ * @returns {Promise<NotificationChannel>}
+ */
 NotificationManager.getNotificationChannel = function(channelId) {
     return new Promise(function(onSuccess, onFail) {
         NotificationManager._getNotificationChannel(channelId, onSuccess, onFail);
     });
 };
 
-
-NotificationManager.openAppNotificationSettings = function(channelId) {
+/**
+ * Open App Notification Settings
+ * @returns {Promise}
+ */
+NotificationManager.openAppNotificationSettings = function() {
     return new Promise(function(onSuccess, onFail) {
-        NotificationManager._openAppNotificationSettings(channelId, onSuccess, onFail);
+        NotificationManager._openAppNotificationSettings(onSuccess, onFail);
     });
 };
 
-
+/**
+ * Open Notification Channel Settings
+ * @param {string} channelId
+ * @returns {Promise}
+ */
 NotificationManager.openNotificationChannelSettings = function(channelId) {
     return new Promise(function(onSuccess, onFail) {
         NotificationManager._openNotificationChannelSettings(channelId, onSuccess, onFail);
     });
 };
 
+/**
+ * Create a new Notification Channel
+ * @param {string} channelId - Unique ID for the channel
+ * @param {string} name - Name of the channel
+ * @param {string} description - Description of the channel
+ * @param {number} importance - Importance level of the channel (see NotificationManager.IMPORTANCE_* constants)
+ * @returns {Promise}
+ */
+NotificationManager.createNotificationChannel = function(channelId, name, description, importance) {
+    return new Promise(function(onSuccess, onFail) {
+        NotificationManager._createNotificationChannel(channelId, name, description, importance, onSuccess, onFail);
+    });
+};
+
+/**
+ * Check if notifications are enabled
+ * @returns {Promise}
+ */
+NotificationManager.areNotificationsEnabled = function() {
+    return new Promise(function(onSuccess, onFail) {
+        NotificationManager._areNotificationsEnabled(onSuccess, onFail);
+    });
+};
+
+/**
+ * Private methods for Cordova exec calls
+ */
 NotificationManager._getNotificationChannel = function(channelId, onSuccess, onFail) {
     cordova.exec(function(channelJSON) {
         onSuccess(new NotificationChannel(channelJSON));
@@ -102,9 +112,12 @@ NotificationManager._openNotificationChannelSettings = function(channelId, onSuc
     }, onFail, NotificationManager.SERVICE_NAME, 'openNotificationChannelSettings', [channelId]);
 };
 
-NotificationManager.areNotificationsEnabled = function(onSuccess, onFail) {
-    cordova.exec(onSuccess, onFail, NotificationManager.SERVICE_NAME, 'areNotificationsEnabled');
+NotificationManager._createNotificationChannel = function(channelId, name, description, importance, onSuccess, onFail) {
+    cordova.exec(onSuccess, onFail, NotificationManager.SERVICE_NAME, 'createNotificationChannel', [channelId, name, description, importance]);
 };
 
+NotificationManager._areNotificationsEnabled = function(onSuccess, onFail) {
+    cordova.exec(onSuccess, onFail, NotificationManager.SERVICE_NAME, 'areNotificationsEnabled');
+};
 
 module.exports = NotificationManager;
